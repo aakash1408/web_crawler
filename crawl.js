615d0cc913e160dc54ed940a9c8f4df9cd643137
aakash1408 <aakashchauhan14082003@gmail.com>
2023-06-05 19:38:18 +0530

    added functionalities to crawl a webpage

--- crawl.js
+++ crawl.js
@@ -1,14 +1,73 @@
+const {JSDOM} = require("jsdom")
 
-function normalizeURL(urlString){
-    const urlObj = new URL(urlString)
-    const hostname = `${urlObj.hostname}${urlObj.pathname}`
-    if(hostname.length > 0 && hostname.slice(-1) == "/"){
-        return hostname.slice(0,-1)
-    }else{
-        return hostname
+async function crawlPage(currentURL){
+    console.log(`crawling page ${currentURL}`)
+    try{
+    const resp = await fetch(currentURL)
+    if (resp.status > 399){
+        console.log(`error in fetch with status code ${resp.status}`)
+        return
+    }
+
+    const contentType = resp.headers.get("content-type")
+    if(!contentType.includes("text/html")){
+        console.log(`error in fetch with status code ${resp.status}`)
+        return
+    }
+
+    console.log(await resp.text())
+    
+    }catch(err){
+        console.log(`error ${err.message} while crawling page ${currentURL}`)
     }
 }
 
+
+function getURLsFromHTML(htmlbody, baseurl){
+    const URLs = []
+    const dom = new JSDOM(htmlbody)
+    const linkElements = dom.window.document.querySelectorAll("a")
+    
+    for (const linkElement of linkElements){
+        if (linkElement.href.slice(0,1) === "/"){
+            //the url is relative
+            try{
+                const urlObj = new URL(`${baseurl}${linkElement.href}`)
+                URLs.push(urlObj.href)
+            }
+            catch (err){
+                console.log(`error with relative url : ${err.message}`)
+            }
+        }
+        else{
+            // the url is absolute
+            try{
+                const urlObj = new URL(linkElement.href)    
+                URLs.push(linkElement.href) 
+            }
+            catch (err){
+                console.log(`error with relative url : ${err.message}`)
+            }  
+        }
+        
+    }
+    return URLs
+}
+
+function normalizeURL(urlString){
+    const urlObj = new URL(urlString)
+    const hostPath = `${urlObj.hostname}${urlObj.pathname}`
+    if (hostPath.length > 0 && hostPath.slice(-1) === "/"){
+        return hostPath.slice(0,-1)
+    }
+    return hostPath
+
+}
+
+
+
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML,
+    crawlPage
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,31 +1,99 @@
 const {test, expect} = require("@jest/globals")
-const { normalizeURL} = require("./crawl.js")
+const { normalizeURL, getURLsFromHTML} = require("./crawl.js")
 
 
-test("normalizeURL strip protocol " , () => {
-const input = 'https://amazon/path'
-const expected = 'amazon/path'
-const actual = normalizeURL(input)
-expect(expected).toEqual(actual)
+test("normalizeURL strip protocol", () => {
+    const input = "https://amazon/path"
+    const expected = "amazon/path"
+    const actual = normalizeURL(input)
+    expect(actual).toEqual(expected)
 })
 
-test("normalizeURL strip trailing / " , () => {
-    const input = 'https://amazon/path/'
-    const expected = 'amazon/path'
+test("normalizeURL strip trailing /", () =>{
+    const input = "https://amazon/path/"
+    const expected = "amazon/path"
     const actual = normalizeURL(input)
-    expect(expected).toEqual(actual)
+    expect(actual).toEqual(expected)
 })
 
 test("normalizeURL capitalize" , () => {
     const input = 'https://AMAZON/path'
     const expected = 'amazon/path'
     const actual = normalizeURL(input)
-    expect(expected).toEqual(actual)
+    expect(actual).toEqual(expected)
 })
 
-test("normalizeURL http " , () => {
-    const input = 'http://amazon/path'
-    const expected = 'amazon/path'
+test("normalizeURL http", ()=>{
+    const input = "http://amazon/path"
+    const expected = "amazon/path"
     const actual = normalizeURL(input)
-    expect(expected).toEqual(actual)
+    expect(actual).toEqual(expected)
 })
+
+test("get Urls from html absolute ", ()=>{
+    const input = `
+    <html>
+        <body>
+            <a href = "https://amazon/">
+                Amazon Shopping
+            </a>
+        </body>
+    </html>    
+    `
+    const inputbaseURL = "https://amazon"
+    const expected = ["https://amazon/"]
+    const actual = getURLsFromHTML(input, inputbaseURL)
+    expect(actual).toEqual(expected)
+})
+
+test("get Urls from html relative ", ()=>{
+    const input = `
+    <html>
+        <body>
+            <a href = "/path/">
+                Amazon Shopping
+            </a>
+        </body>
+    </html>    
+    `
+    const inputbaseURL = "https://amazon"
+    const expected = ["https://amazon/path/"]
+    const actual = getURLsFromHTML(input, inputbaseURL)
+    expect(actual).toEqual(expected)
+})
+
+test("get Urls from html both ", ()=>{
+    const input = `
+    <html>
+        <body>
+            <a href = "https://amazon/">
+                Amazon Shopping
+            </a>
+            <a href = "/path/">
+                Clothes
+            </a>
+        </body>
+    </html>    
+    `
+    const inputbaseURL = "https://amazon"
+    const expected = ["https://amazon/", "https://amazon/path/"]
+    const actual = getURLsFromHTML(input, inputbaseURL)
+    expect(actual).toEqual(expected)
+})
+
+test("get Urls from html invalid ", ()=>{
+    const input = `
+    <html>
+        <body>
+            <a href = "invalid">
+                invalid
+            </a>
+        </body>
+    </html>    
+    `
+    const inputbaseURL = "https://amazon"
+    const expected = []
+    const actual = getURLsFromHTML(input, inputbaseURL)
+    expect(actual).toEqual(expected)
+})
+
